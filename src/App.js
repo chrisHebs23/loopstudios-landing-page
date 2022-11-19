57ad83f1d99fd9450c3bdc49195e7f14e97ac47a
chrisHebs23 <christopher.heberden@gmail.com>
2022-11-19 15:52:48 -0800

    Add hero image and fix nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { lazy, Suspense } from "react";
 
 const Header = lazy(() => import("./Header/Header"));
+const HomePage = lazy(() => import("./pages/HomePages/HomePage"));
 
 const App = () => {
   return (
     <Suspense fallback={<>Loading...</>}>
       <Header />
+      <HomePage />
     </Suspense>
   );
 };
--- src/pages/HomePages/HomePage.js
+++ src/pages/HomePages/HomePage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./components/Home.css";
 
 const HomePage = () => {
-  return <div></div>;
+  return <div className="hero-container"></div>;
 };
 
 export default HomePage;
